65171832a45f58af8e95bece791c76e0cc966f02
Dinanjana <dinanjana@gmail.com>
2020-10-18 19:48:23 +0530

    add user pool info

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_D7vm12o4V', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '75nhi5fmoifaj36iq50egqp9ds', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
